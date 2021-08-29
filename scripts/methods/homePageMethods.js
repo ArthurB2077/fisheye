1a7579ad3dc64397a6b32df6c91fc98d4fca33d6
Arthur Béquié <arthurbequie@gmail.com>
2021-08-29 19:56:49 +0200

    Bugfix : null logged in the console when on phtographe rpage

--- scripts/methods/homePageMethods.js
+++ scripts/methods/homePageMethods.js
@@ -1,21 +1,3 @@
-const openModal = () => {
-  Array.from(document.getElementsByClassName('bg-modal')).forEach(modal => {
-    modal.style.display = 'block'
-  })
-}
-
-const closeModal = () => {
-  Array.from(document.getElementsByClassName('bg-modal')).forEach(modal => {
-    modal.style.display = 'none'
-  })
-}
-
-const logData = () => {
-  for (let i = 0; i < event.target.form.length - 1; i++) {
-    console.log(event.target.form[i].value)
-  }
-}
-
 window.addEventListener('scroll', (event) => {
   if (window.scrollY > 0) {
     document.getElementById('header-content').style.display = 'flex'
